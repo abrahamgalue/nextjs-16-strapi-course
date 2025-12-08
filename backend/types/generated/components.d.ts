7d14505e2494d5d5dc062e2e58d2c912449a3ce0
Abraham Galue <abrahamgalue512@gmail.com>
2025-12-08 11:46:17 -0400

    Connect Strapi with Next.js

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,3 +1,40 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ComponentsLink extends Struct.ComponentSchema {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+    icon: 'paperPlane';
+  };
+  attributes: {
+    href: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'#'>;
+    isExternal: Schema.Attribute.Boolean;
+    label: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    icon: 'alien';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files'> &
+      Schema.Attribute.Required;
+    link: Schema.Attribute.Component<'components.link', false>;
+    subHeading: Schema.Attribute.Text;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'components.link': ComponentsLink;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
+}
